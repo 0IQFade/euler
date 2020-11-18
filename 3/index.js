0a184c1f503448a2b045f9deb315c09b78ac997c
Ryan <fateryan424@gmail.com>
2020-11-17 19:07:02 -0500

    continuation of 3

--- 3/index.js
+++ 3/index.js
@@ -1,18 +1,56 @@
-let max = 600
-let p = 0
+let p = 600851475143
 let total = 0
 let listToMax = [];
 let primeList=[];
-while(p < max){
+let primeFactorList = [];
+
+function isPrime(num){
+    for(let i = 2; i < num; i+= 1) {
+        if(num % i == 0){
+            return false
+        }
+    }
+    return true
+}
+
+function factorsOf(num){
+    let factorsOfNum = [];
+    for(let i = 1; i <= num/2; i+= 2){
+        if(num % i == 0){
+            factorsOfNum.push(i)
+        }
+    }   
+    return factorsOfNum
+}
+
+
+factorsOf(p).forEach(function(num) {
+    p/num
+    if(isPrime(num)){
+        return num;
+    }
+})
+console.dir(num)
+/*console.dir(factorsOf(p))
+factorsOf(p).forEach(function(num) {
+    if(isPrime(num)){
+        primeFactorList.push(num);
+    }
+})
+console.dir(primeFactorList) */
+
+
+
+/*while(p < max){
     p += 1
     total += p
     listToMax.push(p)
 }
 listToMax.forEach((item) => {  
     if(max % item == 0){
-    item += 1
+    
      }
     else primeList.push(item)
 
 })
-console.dir(primeList)
+console.dir(primeList) */
