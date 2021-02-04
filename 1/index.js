42c6642031f5ec6f27ef350a379378faadbf86fd
Ryan Ma <fateryan424@gmail.com>
2021-02-03 22:06:03 -0500

    solved problem 1

--- 1/index.js
+++ 1/index.js
@@ -1,50 +1,16 @@
+
 /*
-let b = "ryan";
-let total = 0;
-console.log(b);
-let myArray = [1, 2, 3, 4, "ryan"];
-
-function add(x, y) {
-    return x + y;
-}
-
-myArray.forEach((item) => {
-    total = add(total, item);
-});
-
-console.log(add(5, 6));
+3,6,9,12,15,18,21
+5,10,15,20,25
 */
+let total = 0
 
-let total = 0;
-const listof3 = [];
 for (let i = 0; i < 1000; i += 3) {
-  total += i;
-  listof3.push(i);
+  total += i
 }
-console.dir(total);
-console.dir(listof3);
-
-let total1 = 0;
-const listof5 = [];
-
-for (let f = 0; f < 1000; f += 5) {
-  total1 += f;
-  listof5.push(f);
-}
-console.log(total1);
-console.dir(listof5);
-
-const listOfCommon = [];
-listof3.forEach((o) => {
-  if (listof5.includes(o)) {
-    listOfCommon.push(o);
+for (let i = 0; i < 1000; i += 5) {
+  if(i % 15 !== 0) {
+    total += i
   }
-});
-console.dir(listOfCommon);
-
-let total2 = 0;
-listOfCommon.forEach((j) => {
-  total2 += j;
-});
-console.log(total2);
-console.log(total + total1 - total2);
+}
+console.log(total)
